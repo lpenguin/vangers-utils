b0116b89adf9e4b54d8590c301136ec1f7c7a3be
nikita prianichnikov <lpenguin@yandex.ru>
2016-09-29 15:06:34 +0300

    scb refactor

--- vangers_utils/cli/scb2yaml.py
+++ vangers_utils/cli/scb2yaml.py
@@ -3,10 +3,17 @@
     -h, --help
 """
 from typing import Dict
-from vangers_utils.scb.decode.convert import ScbToYamlConverter
+
+from vangers_utils.scb.decode.convert import ScbToYamlEncoder
+
+
+def _decode(in_filename: str, out_filename: str):
+    with open(in_filename, 'rb') as in_f:
+        with open(out_filename, 'wt') as out_f:
+            ScbToYamlEncoder(in_f, out_f).convert()
 
 
 def main(args: Dict[str, str]):
-    ScbToYamlConverter(args['<in>'], args['<out>']).convert()
-
-
+    in_filename = args['<in>']
+    out_filename = args['<out>']
+    _decode(in_filename, out_filename)

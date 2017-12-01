c889cac0840ebe3b779c5fd4d88be979d9b46bb2
nikita prianichinkov <lpenguin@yandex.ru>
2017-12-01 15:59:54 +0300

    added palette files to package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vangers-utils',
-    version='0.1.0',
+    version='0.2.1',
     packages=['vangers_utils', 'vangers_utils.cli', 'vangers_utils.scb', 'vangers_utils.scb.decode',
               'vangers_utils.scb.encode', 'vangers_utils.image', 'vangers_utils.image.bmp', 'vangers_utils.image.xbm'],
     url='https://github.com/lpenguin/vangers-utils',
@@ -13,5 +13,8 @@ setup(
     install_requires=['pyyaml', 'numpy', 'Pillow', 'typing', 'docopt'],
     entry_points={
         'console_scripts': ['vangers-utils=vangers_utils.cli.main:main'],
-    }
+    },
+    include_package_data=True,
+    zip_safe=False
+
 )
